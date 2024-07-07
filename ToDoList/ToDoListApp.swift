1724c002663e7c4fa3a96917e4e278bb7e63f960
Katja Rübel <Katja.ruebel@gmail.com>
2024-07-07 19:43:58 +0200

    Added Firebase to the App to use database and authentication

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -5,10 +5,15 @@
 //  Created by katja.ruebel on 07.07.24.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
