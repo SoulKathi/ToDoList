eefbf53d1f50aec1132164a174e3b86bea7d68b0
Katja Rübel <Katja.ruebel@gmail.com>
2024-07-11 13:17:46 +0200

    Improved/Created structure of what is displayed after login

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class NewItemViewViewModel {
+    init() {
+        
+    }
+}
--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -12,10 +12,23 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            // Signed in
-            ToDoListView()
+            accountView            
         } else {
             LogInView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
         }
     }
 }
