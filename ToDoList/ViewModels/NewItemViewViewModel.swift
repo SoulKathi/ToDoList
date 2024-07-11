4c40f7131c37e0921320528066a666776dbe06c2
Katja Rübel <Katja.ruebel@gmail.com>
2024-07-11 22:36:04 +0200

    Items are now saved in the DB

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by katja.ruebel on 07.07.24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -18,7 +20,26 @@ class NewItemViewViewModel: ObservableObject {
     }
     
     func save() {
+        guard canSafe else {
+            return
+        }
         
+        // Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false)
+        
+        // Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSafe: Bool {
